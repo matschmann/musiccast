a01968cea329a9bcbbebffa042826000bfd81d4c
Rainer Standke <rainer.standke@deutschebahn.com>
2020-05-20 05:42:58 +0200

    Init gradle project with Java 14

---
+++ src/main/java/net/rainr/musiccast/MusiccastApplication.java
@@ -0,0 +1,13 @@
+package net.rainr.musiccast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccastApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusiccastApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/rainr/musiccast/MusiccastApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rainr.musiccast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiccastApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
